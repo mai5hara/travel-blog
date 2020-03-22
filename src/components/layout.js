06412515e5a7503dbd255cc502dcbcd8894352b5
maigohara <maigohara-1103@outlook.jp>
2020-03-22 16:06:41 -0700

    add files

---
+++ src/components/layout.js
@@ -0,0 +1,89 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { rhythm, scale } from "../utils/typography"
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  let header
+
+  if (location.pathname === rootPath) {
+    header = (
+      <h1
+        style={{
+          ...scale(1),
+          marginBottom: rhythm(1.5),
+          marginTop: 0,
+        }}
+      >
+      <nav>
+        
+      </nav>
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+    )
+  }
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`
+      }}
+    >
+      <header
+        style={{
+          maxWidth: rhythm(60),
+          padding: `${rhythm(1)} ${rhythm(3 / 4)}`,
+        }}
+      >  
+        {header}
+      </header>
+      <main
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        {children}
+      </main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,99 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
+import styled from "styled-components"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+const Post = styled.div`
+  display: flex;
+`
+
+const PostImage = styled.div`
+  flex: 25%;
+  margin-right: 1rem
+`
+
+const PostText = styled.div`
+  flex: 75%;
+`
+
+class BlogIndex extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allContentfulPost.edges
+    console.log(posts)
+  
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO 
+          title="Travel blog"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+        <Bio />
+        {posts.map(({ node }) => {
+          const title = node.title || node.slug
+          return (
+            <Post key={node.slug}>
+              <PostImage>
+                <Img fluid={node.image.fluid} />
+              </PostImage>
+              <PostText>
+                <header>
+                  <h3
+                    style={{
+                      marginTop: 0,
+                      marginBottom: rhythm(1 / 4),
+                    }}
+                  >
+                    <Link style={{ boxShadow: `none` }} to={node.slug}>
+                      {title}
+                    </Link>
+                  </h3>
+                </header>
+                <section>
+                  <p>{node.subtitle}</p>
+                </section>
+              </PostText>
+            </Post>
+          )
+        })}
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        author {
+          name
+          summary
+        }
+      }
+    }
+    allContentfulPost {
+      edges {
+        node {
+          title
+          subtitle
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+          author
+          slug
+        }
+      }
+    }
+  }
+`
